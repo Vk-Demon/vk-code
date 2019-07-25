b65aa64b6f06c4e360c1601403a01964a2d8b6c8
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-07-25 20:55:45 +0530

    Create ckbegs439.py

---
+++ ckbegs439.py
@@ -0,0 +1,3 @@
+nump,numq,numr,numa,numb,numc,numd,nume,numg,numg=input().split()
+nump,numq,numr,numa,numb,numc,numd,nume,numg,numg=int(nump),int(numq),int(numr),int(numa),int(numb),int(numc),int(numd),int(nume),int(numg),int(numg)
+print(max(nump,numq,numr,numa,numb,numc,numd,nume,numg,numg))

304609cbfb4249ce29f059cc80a8d56535fcddc9
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-08-07 20:07:33 +0530

    Create ckbegs658.py

---
+++ ckbegs658.py
@@ -0,0 +1,11 @@
+nnum,knum=input().split()
+nnum,knum=int(nnum),int(knum)
+ary=[int(i) for i in input().split()]
+rpc=0
+for i in range(0,nnum):
+  if(knum==ary[i]):
+    rpc=rpc+1
+if(rpc>0):
+  print("yes") 
+else:
+  print("no")

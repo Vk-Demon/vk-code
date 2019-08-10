109dcbae0ff1bfc86d2b83e1d6f01f849edc1f53
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-08-10 21:46:18 +0530

    Update ckbegs875.py

--- ckbegs875.py
+++ ckbegs875.py
@@ -2,8 +2,14 @@ sstr=input()
 mda=int(len(sstr)/2)
 mdb=mda-1
 if(len(sstr)%2==0):
-  sstr=sstr.replace(sstr[mdb],"*",1)
-  sstr=sstr.replace(sstr[mda],"*",1)
+  for i in range(0,len(sstr)):
+    if(i==mdb or i==mda):
+      print("*",end="")
+    else:
+      print(sstr[i],end="")  
 else:
-  sstr=sstr.replace(sstr[mda],"*",1)
-print(sstr)
+  for i in range(0,len(sstr)):
+    if(i==mda):
+      print("*",end="")
+    else:
+      print(sstr[i],end="") 

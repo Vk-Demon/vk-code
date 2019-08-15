33036b328aa040c8bfbe32ee75f9da4a5b5450c0
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-08-15 14:14:18 +0530

    Create ckbegs879.py

---
+++ ckbegs879.py
@@ -0,0 +1,12 @@
+xnum,ynum=input().split()
+xnum,ynum=int(xnum),int(ynum)
+pnum=xnum*ynum
+s=0
+for i in range(1,pnum+1):
+  q=i*i
+  if(q==pnum):
+    s=s+1
+if(s==1):
+  print("yes")
+else:
+  print("no")

24734ad011d35bfd484e0e23e1d02a34d63bbb35
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-08-27 23:29:37 +0530

    Update cknumber5.py

--- cknumber5.py
+++ cknumber5.py
@@ -4,7 +4,7 @@ s,c=0,0
 for i in range(0,2):
   s=s+int(ngk[i])
 for i in range(0,len(ng)-1):
-  if(int(ng[i]+ng[i+1])==s or int(ng[i])==s):
+  if(int(ng[i]+ng[i+1])==s or int(ng[i])==s or int(ng[i+1])==s):
     c=c+1
 if(c>0):
   print("1")

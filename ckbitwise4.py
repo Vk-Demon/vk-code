e41581c5a6f889e241ebdefe0bb366bd1b8f9649
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-08-30 21:38:46 +0530

    Create ckbitwise4.py

---
+++ ckbitwise4.py
@@ -0,0 +1,3 @@
+rtnum,rtshift=input().split()          # Given 2 numbers N,K print the number after performing right shift 'K' times(upto 2 decimal places)
+rtnum,rtshift=int(rtnum),int(rtshift)
+print(rtnum>>rtshift)

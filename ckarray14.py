1d927ce43cd2bc9f9f676f5b0f77288cb3ed7d5b
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-08-31 22:08:05 +0530

    Create ckarray14.py

---
+++ ckarray14.py
@@ -0,0 +1,12 @@
+knum=int(input())   # Find the count k by which array has been rotated in the rotated sorted array. Given a number N followed 2 arrays A and B of length N. Find the amount K by which the array has been rotated
+lt=[int(i) for i in input().split()]
+lt2=[int(i) for i in input().split()]
+c1,c2=0,0
+for i in range(0,len(lt2)):
+  if(lt2[i]==knum):
+    c1=i
+for i in range(0,len(lt)):
+  if(lt[i]==knum):
+    c2=i
+c3=(len(lt)-c2)-c1
+print(c3)

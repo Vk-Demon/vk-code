f30a5f3b2e8220d6a540a6cf908cc203f486ae21
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-09-05 20:12:12 +0530

    Create ckalgorithm1.py

---
+++ ckalgorithm1.py
@@ -0,0 +1,9 @@
+nnum=int(input())  # Given a number N followed by an array of N integers, every element appears twice except for one. Find that single one and print it.
+lt=[int(i) for i in input().split()]
+for i in range(0,nnum):
+  cq=0
+  for j in range(0,nnum):
+    if(lt[i]==lt[j]):
+      cq=cq+1
+  if(cq==1):
+    print(lt[i],end=" ")

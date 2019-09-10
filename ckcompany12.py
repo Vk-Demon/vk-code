4af6e8be832c0fec5949f6abc3ca39fc48f21086
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2019-09-10 20:04:32 +0530

    Create ckcompany12.py

---
+++ ckcompany12.py
@@ -0,0 +1,3 @@
+anum,bnum,xnum=input().split() # Given the values of 'a' and 'b' and one value of 'x' in the equation ax + b = y. Find the value of y.
+anum,bnum,xnum=int(anum),int(bnum),int(xnum)
+print(anum*xnum+bnum)
